fb3bc6083dc99fddbb865a78a6176f4994da7db5
Daman Bual <damanbual@gmail.com>
2024-09-25 16:51:38 -0700

    addded suggesstion from ai submission

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import FavIcon from './FavIcon'; // Assuming you have a FavIcon component
+import FavIcon from './FavIcon';
 import '../styles/FavBadge.scss';
 
-const FavBadge = ({ isFavPhotoExist }) => {
+const FavBadge = ({ isFavPhotoExist, className }) => {
   return (
-    <div className='fav-badge'>
-      <FavIcon displayAlert={!!isFavPhotoExist} />  {/* Display the heart icon */}
-      {/* You can add more logic for a counter or something else */}
+    <div className={`fav-badge ${className}`}> {/* Applied the className prop here */}
+      <FavIcon displayAlert={!!isFavPhotoExist} />
     </div>
   );
 };
